b37fe2bede2797ee7cfa32200bc6421173e36959
abdelalibakka <pakkatatata@gmail.com>
2023-08-16 17:44:00 -0700

    tasks

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,4 +1,3 @@
--- Showing all_privileges_of the_MySQL users user_0d_1& user_0d_2 on_server.
-
+--  lists all privileges of the MySQL users
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,11 +1,10 @@
--- Creates the database hbtn_0d_2
-CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+-- Create the database hbtn_0d_2 and the user user_0d_2
 
--- Creates the user user_0d_2
+-- Create Databse
+CREATE DATABASE IF NOT EXISTS `hbtn_0d_2`;
+
+-- Create user
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 
--- Grants SELECT privilege on hbtn_0d_2 database to user_0d_2
-GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
-
--- Reloads the grant tables to apply the changes
-FLUSH PRIVILEGES;
+-- Grant SELECT privileges on database
+GRANT SELECT ON `hbtn_0d_2`.* TO 'user_0d_2'@'localhost';
