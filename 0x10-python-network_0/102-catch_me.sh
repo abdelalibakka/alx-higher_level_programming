266c4a25e48d3ee4cf297cde20c7609f1f5c4c0f
abdelalibakka <pakkatatata@gmail.com>
2023-10-02 15:02:54 -0700

    102-catch_me.sh

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-#makes a request to 0.0.0.0:5000/catch_me
-curl -s 0.0.0.0:5000/catch_me -w 'You got me!' -o /dev/null
+# Maks_a_request to 0.0.0.0:5000/catch_me that_gets_message.
+curl -sL -X PUT -H "Origin: HolbertonSchool" -d "user_id=98" 0.0.0.0:5000/catch_me
